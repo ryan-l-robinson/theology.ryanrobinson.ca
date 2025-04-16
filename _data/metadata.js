2a8e5119fa416c2f5a64953e4fc64dd1dae47a09
Ryan Robinson <ryan@ryanrobinson.ca>
2025-04-16 17:04:08 -0400

    Updated handling of title

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,7 +1,7 @@
 export default {
 	title: "Ryan Robinson Theology",
 	url: "https://theology.ryanrobinson.ca/",
-	language: "en",
+	language: "en-ca",
 	site_description: "A guy with a little theological training writing about God, the church, and the world.",
 	author: {
 		name: "Ryan Robinson",
