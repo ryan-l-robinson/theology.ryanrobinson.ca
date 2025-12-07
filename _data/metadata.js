04e88ac1d5d07495b22f4381ce02c7e9ec6aa97d
Ryan Robinson <ryan@ryanrobinson.ca>
2025-12-07 15:56:23 -0500

    Differentiated theology icon

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,10 +1,12 @@
 export default {
 	title: "Ryan Robinson Theology",
 	url: "https://theology.ryanrobinson.ca/",
+	link_icon: "theology-blog",
 	language: "en-ca",
-	site_description: "A guy with a little theological training writing about God, the church, and the world.",
+	site_description:
+		"A guy with a little theological training writing about God, the church, and the world.",
 	author: {
 		name: "Ryan Robinson",
-		url: "https://ryanrobinson.ca"
-	}
-}
+		url: "https://ryanrobinson.ca",
+	},
+};
