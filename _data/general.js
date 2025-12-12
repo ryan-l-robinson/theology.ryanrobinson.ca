14ce16c8ebf7f5188a7918fe13b28eed7e190f62
Ryan Robinson <ryrobinson@wlu.ca>
2025-12-12 13:46:58 -0500

    Changed variable structure

--- _data/general.js
+++ _data/general.js
@@ -3,9 +3,6 @@ export default {
 	title: "Ryan Robinson Theology",
 	url: "https://theology.ryanrobinson.ca/",
 	link_icon: "theology-blog",
-	language: "en-ca",
-	region: "CA-ON",
-	fediverse: "@Ryan@mstdn.ca",
 	description:
 		"A guy with a little theological training writing about God, the church, and the world.",
 	author: {
---
+++ _data/meta.js
@@ -0,0 +1,6 @@
+// Variables that can be inserted into templates.
+export default {
+	language: "en-ca",
+	region: "CA-ON",
+	fediverse: "@Ryan@mstdn.ca",
+};
